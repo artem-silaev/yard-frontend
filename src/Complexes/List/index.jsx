83f9af65926717e77dedeee196b5b3caa76f9881
Artem Silaev <artem@silaev.net>
2017-07-06 09:58:24 +0300

    add links to complexes

--- src/Complexes/List/index.jsx
+++ src/Complexes/List/index.jsx
@@ -17,21 +17,21 @@ export default () => (
         <Discover />
           <div className="cards">
             <Card
-              url="#"
+              url="/complex/1"
               location={"South Beach, San Francisco"}
               address={"764 Metropolitan Avenue"}
               description={"The Lewis Steel Building is a masterful industrial conversion located in the heart of Williamsburg. Located at 76 North 4th Street, the former 1930's steel factory has been transformed into 83 individually unique and luxury loft apartments."}
               image={"./images/bitmap1.png"}
             />
             <Card
-              url="#"
+              url="/complex/2"
               location={"South Beach, San Francisco"}
               address={"764 Metropolitan Avenue"}
               description={"The Lewis Steel Building is a masterful industrial conversion located in the heart of Williamsburg. Located at 76 North 4th Street, the former 1930's steel factory has been transformed into 83 individually unique and luxury loft apartments."}
               image={"./images/bitmap1.png"}
             />
             <Card
-              url="#"
+              url="/complex/3"
               location={"South Beach, San Francisco"}
               address={"764 Metropolitan Avenue"}
               description={"The Lewis Steel Building is a masterful industrial conversion located in the heart of Williamsburg. Located at 76 North 4th Street, the former 1930's steel factory has been transformed into 83 individually unique and luxury loft apartments."}
